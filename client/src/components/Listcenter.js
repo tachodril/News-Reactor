3d90b95a7ba217d54934c1d8eae935e32ed69849
tachodril <rajlevel97@gmail.com>
2020-07-24 16:48:50 +0530

    posts added to store for all tabs

--- client/src/components/Listcenter.js
+++ client/src/components/Listcenter.js
@@ -7,21 +7,48 @@ class Listcenter extends Component {
   constructor(props) {
     super(props);
 
+    this.fetchNewsFunction = this.fetchNewsFunction.bind(this);
+
     var flags = [];
     flags = props.isLoaded;
     var flag = flags[props.curTab];
     if (!flag) {
-      var catUrl = "";
-      if (props.curTab === 0) {
-        catUrl =
-          "https://newsapi.org/v2/top-headlines?country=in&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
-      }
-      var posts = [];
-      axios.get(catUrl).then((res) => {
-        posts = res.data.articles;
-        props.fetchArticles(posts);
-      });
+      this.fetchNewsFunction(props.curTab);
     }
+  }
+
+  fetchNewsFunction(cat) {
+    var catUrl = "";
+    if (cat === 0) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    } else if (cat === 1) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    } else if (cat === 2) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    } else if (cat === 3) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    } else if (cat === 4) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    } else if (cat === 5) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    } else if (cat === 6) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    } else if (cat === 7) {
+      catUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=37fb26157b3b48dd80f9ef8a891e1374";
+    }
+    var posts = [];
+    axios.get(catUrl).then((res) => {
+      posts = res.data.articles;
+      this.props.fetchArticles(posts, cat);
+    });
   }
 
   render() {
@@ -54,17 +81,37 @@ class Listcenter extends Component {
 }
 
 const mapStateToProps = (state) => {
+  var posts;
+  var tab = state.curTab;
+  if (tab === 0) {
+    posts = state.articles1;
+  } else if (tab === 1) {
+    posts = state.articles2;
+  } else if (tab === 2) {
+    posts = state.articles3;
+  } else if (tab === 3) {
+    posts = state.articles4;
+  } else if (tab === 4) {
+    posts = state.articles5;
+  } else if (tab === 5) {
+    posts = state.articles6;
+  } else if (tab === 6) {
+    posts = state.articles7;
+  } else if (tab === 7) {
+    posts = state.articles8;
+  }
+
   return {
-    articles: state.articles1,
-    curTab: state.curTab,
+    articles: posts,
+    curTab: tab,
     isLoaded: state.isLoaded,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchArticles: (articles) => {
-      dispatch({ type: "FETCH_ARTICLES", articles: articles });
+    fetchArticles: (articles, cat) => {
+      dispatch({ type: "FETCH_ARTICLES", articles: articles, cat: cat });
     },
   };
 };
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -29,8 +29,10 @@ const initState = {
 
 const rootReducer = (state = initState, action) => {
   if (action.type === "FETCH_ARTICLES") {
+    var curCat = action.cat;
+
     var flags = state.isLoaded;
-    flags[state.curTab] = true;
+    flags[curCat] = true;
     // const ind = state.curTab;
     // console.log("heyy " + flags);
 
@@ -45,11 +47,55 @@ const rootReducer = (state = initState, action) => {
 
     // console.log("level " + posts);
 
-    return {
-      ...state,
-      isLoaded: flags,
-      articles1: action.articles,
-    };
+    if (curCat === 0) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles1: action.articles,
+      };
+    } else if (curCat === 1) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles2: action.articles,
+      };
+    } else if (curCat === 2) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles3: action.articles,
+      };
+    } else if (curCat === 3) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles4: action.articles,
+      };
+    } else if (curCat === 4) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles5: action.articles,
+      };
+    } else if (curCat === 5) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles6: action.articles,
+      };
+    } else if (curCat === 6) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles7: action.articles,
+      };
+    } else if (curCat === 7) {
+      return {
+        ...state,
+        isLoaded: flags,
+        articles8: action.articles,
+      };
+    }
   }
 
   return state;
